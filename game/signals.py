9126f11576a7d4355b8602bc48f1fd9240ff1f93
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-12 06:09:37 +0530

    commit

--- game/signals.py
+++ game/signals.py
@@ -9,19 +9,10 @@ def create_player_profile(sender, instance, created, **kwargs):
     """Create PlayerProfile for new users only if one doesn't exist"""
     if created:
         with atomic():
-            # Get first mission by order
-            first_mission = Mission.objects.order_by('order').first()
-            
             # Create profile if it doesn't exist
             profile, created = PlayerProfile.objects.get_or_create(
-                user=instance,
-                defaults={'current_mission': first_mission}
+                user=instance
             )
-            
-            # Ensure current_mission is set
-            if not profile.current_mission:
-                profile.current_mission = first_mission
-                profile.save(update_fields=['current_mission'])
 
 @receiver(post_save, sender=User)
 def save_player_profile(sender, instance, **kwargs):
