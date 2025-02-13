ba0bd83f8117038d8c14f437c6acf76d67c8c690
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-13 05:55:27 +0530

    commit

--- game/models.py
+++ game/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,12 +7,6 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """Create or update user profile when User is created/updated"""
-    if created:
-        UserProfile.objects.create(user=instance)
 
 class Mission(models.Model):
     title = models.CharField(max_length=255)
--- game/signals.py
+++ game/signals.py
@@ -1,21 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from django.db.transaction import atomic
-from .models import PlayerProfile, Mission
+from .models import UserProfile, Mission
 
 @receiver(post_save, sender=User)
-def create_player_profile(sender, instance, created, **kwargs):
-    """Create PlayerProfile for new users only if one doesn't exist"""
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create or update user profile when User is created/updated"""
     if created:
-        with atomic():
-            # Create profile if it doesn't exist
-            profile, created = PlayerProfile.objects.get_or_create(
-                user=instance
-            )
+        UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_player_profile(sender, instance, **kwargs):
-    """Save PlayerProfile if it exists"""
-    if hasattr(instance, 'playerprofile'):
-        instance.playerprofile.save()
+def save_user_profile(sender, instance, **kwargs):
+    """Save the user profile"""
+    if hasattr(instance, 'userprofile'):
+        instance.userprofile.save()
