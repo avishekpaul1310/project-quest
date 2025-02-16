9905ff02b7eace0d6a6159f6921a4d9d15a673e3
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-16 10:01:01 +0530

    commit

--- project_quest/urls.py
+++ project_quest/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from game.views import CustomLogoutView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('game.urls', 'game'), namespace='game')),
-    path('login/', auth_views.LoginView.as_view(template_name='game/login.html'), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('', include('game.urls')),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='game/login.html',
+        redirect_authenticated_user=True
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='game/logout.html',
+        next_page='login'
+    ), name='logout'),
 ]
